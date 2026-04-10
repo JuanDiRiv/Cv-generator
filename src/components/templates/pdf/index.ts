9a2e255e7708698353263beb7bb106c75295e63d
JuanDiRiv <juandirivero@gmail.com>
2026-04-10 11:42:50 -0500

    feat: add hyphenation callback to prevent word splitting in PDF templates

--- src/components/templates/pdf/index.ts
+++ src/components/templates/pdf/index.ts
@@ -1,8 +1,12 @@
+import { Font } from '@react-pdf/renderer'
 import { BudapestPDF } from './BudapestPDF'
 import { MinimalPDF } from './MinimalPDF'
 import { ModernPDF } from './ModernPDF'
 import { ExecutivePDF } from './ExecutivePDF'
 import type { TemplateId, CVDocument } from '@/types/cv'
+
+// Disable automatic hyphenation to avoid splitting long words with '-'.
+Font.registerHyphenationCallback((word) => [word])
 
 export const pdfTemplates: Record<TemplateId, React.ComponentType<{ cv: CVDocument }>> = {
   budapest: BudapestPDF,
